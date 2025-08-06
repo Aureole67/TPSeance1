06ac34d246653bfb32eaa2f28b5238e90b298354
Aureole <aurolouka@gmail.com>
2025-08-06 23:51:47 +0100

    Prjet car

---
+++ src/js/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.html", "./src/**/*.{js,jsx,ts,tsx,vue,php,blade.php}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: ['Playfair Display', 'serif'],
+        syne: ['"Syne"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
